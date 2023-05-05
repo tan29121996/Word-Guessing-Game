0eba61fa38d0b032ec7a5138a2ce1eb5808e8aef
tan29121996 <88268203+tan29121996@users.noreply.github.com>
2023-05-05 20:15:11 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore, initializeFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADAsnhnHu_6W9FJDMfYsWev1_SBOobLDs",
-  authDomain: "whatchaknow-4ab97.firebaseapp.com",
-  projectId: "whatchaknow-4ab97",
-  storageBucket: "whatchaknow-4ab97.appspot.com",
-  messagingSenderId: "31009074676",
-  appId: "1:31009074676:web:26c6c40b4f2192f13e9da1",
-  measurementId: "G-VYJYTBY4DE"
+
 };
 
 const app = initializeApp(firebaseConfig);
